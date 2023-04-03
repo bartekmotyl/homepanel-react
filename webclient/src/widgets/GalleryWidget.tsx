925699142cc704089c27655d3d4160080cd3d77c
bartekmotyl <29923773+bartekmotyl@users.noreply.github.com>
2023-04-03 21:48:51 +0200

    GalleryWidget

--- webclient/src/widgets/GalleryWidget.tsx
+++ webclient/src/widgets/GalleryWidget.tsx
@@ -1,36 +1,83 @@
-import React, { useState } from 'react';
-import { WidgetProperties } from './widgets';
-import styled from 'styled-components';
-import {useAsync, useInterval} from 'react-use';
-import { useSwipeable } from 'react-swipeable';
-//import Image from 'material-ui-image'
+import React, { useState } from 'react'
+import { WidgetProperties } from './widgets'
+import styled from 'styled-components'
+import {useAsync, useInterval} from 'react-use'
+import { useSwipeable } from 'react-swipeable'
+import { DateTime } from 'luxon'
+import { random } from 'lodash'
+
+type PhotoGroupMeta = {
+  name: string
+  photosCount: number
+}
+
+type PhotoData = {
+  data: Buffer | string
+  contentType: string
+  fileName: string
+  ctime: Date
+  mtime: Date
+  exifDate: string
+}
 
 export function GalleryWidget({ props }: WidgetProperties) {
   const [hash, setHash] = useState(Date.now())
+
   const [isLoading, setIsLoading] = useState(false)
   const [selectedGroup, setSelectedGroup] = useState(0)
+  const [groups, setGroups] = useState<PhotoGroupMeta[]>([])
 
-  const state = useAsync(async () => {
-      const origUrl: string = props.src
-      const url = origUrl.replace("/0", `/${selectedGroup}`)
+  const [currentPhoto, setCurrentPhoto] = useState<PhotoData | undefined>()
+  const [currentGroupName, setCurrentGroupName] = useState<string | undefined>()
+
+  useAsync(async () => {
+    const url = `${props.src}/photos/groups`
+    const response = await fetch(url)
+    const result = await response.json() as PhotoGroupMeta[]
+    setGroups(result)
+  }, []);
+
+ 
+
+  useAsync(async () => {
+      // we assume here 
+      console.log(`selectedGroup: ${selectedGroup}`)
+      let idx = selectedGroup - 1
+      let auto = false
+      if (idx === -1) {
+        idx = random(groups.length-1)
+        auto = true
+      } 
+      const url = `${props.src}/photos/random/json/${idx}`
       setIsLoading(true)
-      const response = await fetch(url);
-      const result = await response.json();
+      const response = await fetch(url)
+      const result = await response.json() as PhotoData
+      setCurrentPhoto(result)
+      setCurrentGroupName(auto ? `${groups[idx].name} (*)` : `${groups[idx].name}`)
       setIsLoading(false)
-      return result
-  }, [hash, selectedGroup]);
+  }, [groups, hash, selectedGroup]);
     
   const handleClick = () => {
     setHash(Date.now())
   }    
 
-  const groupCount = 2
+  const exifDate = () => {
+    if (!currentPhoto?.exifDate) {
+      return ''
+    }
+    const date = DateTime.fromFormat(currentPhoto?.exifDate, "yyyy:MM:dd HH:mm:ss")
+    return date.toFormat("yyyy/MM/dd")
+  }
   const handlers = useSwipeable({
     onSwipedLeft: (_eventData) => {
-      setSelectedGroup((selectedGroup + 1) % groupCount)
+      if (groups.length > 0) {
+        setSelectedGroup((selectedGroup + (groups.length + 1) - 1) % (groups.length + 1))
+      }
     },
     onSwipedRight: (_eventData) => { 
-      setSelectedGroup((selectedGroup + groupCount - 1) % groupCount)
+      if (groups.length > 0) {
+        setSelectedGroup((selectedGroup + 1) % (groups.length + 1))
+      }
     },
 })
   
@@ -39,8 +86,9 @@ export function GalleryWidget({ props }: WidgetProperties) {
 
   return (
     <StyledImageContainer {...handlers} >
-      <Box>{state.value?.exifDate ?? ''}</Box>
-      <StyledImage src={`data:image/jpeg;base64, ${state.value?.data ?? ''}`} alt='' onClick={handleClick} isLoading={isLoading}/>
+      <BoxGroup>{currentGroupName ?? ''}</BoxGroup>
+      <BoxDate>{exifDate()}</BoxDate>
+      <StyledImage src={`data:image/jpeg;base64, ${currentPhoto?.data ?? ''}`} alt='' onClick={handleClick} isLoading={isLoading}/>
     </StyledImageContainer>
   )
 }
@@ -48,6 +96,7 @@ export function GalleryWidget({ props }: WidgetProperties) {
 const StyledImageContainer = styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
 `
 const StyledImage = styled.img<{isLoading: boolean}>`
   width: auto;
@@ -58,13 +107,24 @@ const StyledImage = styled.img<{isLoading: boolean}>`
   opacity: ${props =>  `${props.isLoading ? "30%" : "100%"}`};
 `
 
-const Box = styled.div`
+const BoxDate = styled.div`
   background-color: rgba(76, 76, 76, 0.6);
   color: white;
   position: absolute;
   padding: 0.6rem;
-  left: 100;
-  top: 100;
+  left: 0;
+  top: 0;
+  font-size: 1rem;
+  font-weight: bold;
+`
+
+const BoxGroup = styled.div`
+  background-color: rgba(76, 76, 76, 0.6);
+  color: white;
+  position: absolute;
+  padding: 0.6rem;
+  right: 0;
+  top: 0;
   font-size: 1rem;
   font-weight: bold;
 `
