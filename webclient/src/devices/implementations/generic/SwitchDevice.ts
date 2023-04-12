b6f67239b4a59b5562fd2b43597ead9de698721f
bartekmotyl <29923773+bartekmotyl@users.noreply.github.com>
2023-04-12 21:26:05 +0200

    cleanup

--- webclient/src/devices/implementations/generic/SwitchDevice.ts
+++ webclient/src/devices/implementations/generic/SwitchDevice.ts
@@ -1,17 +1,18 @@
 import { ConnectedDeviceBase } from "../../Device"
-import { Switch  } from "../../interfaces/generic/genericDevices"
-import { store } from '../../../app/store'
+import { Switch } from "../../interfaces/generic/genericDevices"
+import { store } from "../../../app/store"
 
-export class SwitchDevice extends ConnectedDeviceBase implements  Switch {
+export class SwitchDevice extends ConnectedDeviceBase implements Switch {
   toggle(): void {
-    store.dispatch({ type: `connector/${this.connectorId}/device/switch/toggle`, payload: {
-      deviceId: this.deviceId,
-    }})
+    store.dispatch({
+      type: `connector/${this.connectorId}/device/switch/toggle`,
+      payload: {
+        deviceId: this.deviceId,
+      },
+    })
   }
-  off(): void {
-  }
-  on(): void {
-  }
+  off(): void {}
+  on(): void {}
   getState(): boolean | null {
     return this.data?.state
   }
